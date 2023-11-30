303bc5d65b7765e7872e164db3bf8be4f2e33d1a
Kevin C. Krinke <kevin@krinke.ca>
2023-11-30 13:16:35 -0500

    site-theme: adding go supports for semantic-enjin-site theme

---
+++ site-local.go
@@ -0,0 +1,33 @@
+//go:build (fs_theme && (drivers_fs_local || drivers_fs || drivers || locals) && !drivers_fs_embed && !embeds) || all
+
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package theme
+
+import (
+	"path/filepath"
+	"runtime"
+
+	"github.com/go-enjin/be/features/fs/themes"
+)
+
+func SiteTheme() themes.Feature {
+	_, fn, _, _ := runtime.Caller(0)
+	path, _ := filepath.Abs(filepath.Join(filepath.Dir(fn), SiteName))
+	return themes.
+		NewTagged(SiteName).
+		LocalTheme(path).
+		Make()
+}
